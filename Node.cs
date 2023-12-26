813065fc90db7422ffcfe3bb0ae9c03106efb690
itishree2 <nayakitishree6262@gmail.com>
2023-12-26 19:07:05 +0530

    fist commited.

---
+++ Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Selection4._9
+{
+    public class Node
+    {
+        private string data;
+        private Node next = null;
+        private Node prev = null;
+
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+
+        public Node Prev
+        {
+            get { return this.prev; }
+            set { this.prev = value; }
+        }
+    }
+}
+
+
+
